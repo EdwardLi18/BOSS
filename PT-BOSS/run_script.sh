264717836de63f2e8221716044efc77c8ce20921
Edward Li <LegendEddie18@gmail.com>
2020-12-31 12:22:42 -0500

    Change run_script arguments back to original

--- PT-BOSS/run_script.sh
+++ PT-BOSS/run_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python3 train.py  --n-labeled 10 --n-epochs 512 --batchsize 32 --mu 7 --thr 0.95 --lam-u 1 --lr 0.04 --weight-decay 5e-4 --momentum 0.85 --seed 6 --balance 4 --cuda-enabled 1
+CUDA_VISIBLE_DEVICES=0 python train.py  --n-labeled 10 --n-epochs 512 --batchsize 32 --mu 7 --thr 0.95 --lam-u 1 --lr 0.04 --weight-decay 5e-4 --momentum 0.85 --seed 6 --balance 4 --cuda-enabled 1
